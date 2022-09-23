42ac7769081c6170404f6d5433019eafe79e91b5
victor.dsilva <victor.moraesrj@gmail.com>
2022-09-23 11:53:15 -0300

    Nav bottom responsiva e altura de edittext fixados.

--- app/src/main/java/br/pro/moraes/burguerassesement/MainActivity.kt
+++ app/src/main/java/br/pro/moraes/burguerassesement/MainActivity.kt
@@ -8,8 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
 
     }
 }
